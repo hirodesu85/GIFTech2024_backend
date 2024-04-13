fcaed8be1a3ada2b7d54e3017ba376043aa6a5f5
Yukky <67864326+Yoshino-Yukitaro@users.noreply.github.com>
2024-04-13 14:48:22 +0900

    場所関連のAPIを統合

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -1,21 +1,34 @@
-module Api
-  class PlacesController < ApplicationController
-    def suggest
-      # クエリパラメータから値を取得
-      category = params[:category]
-      distance = params[:distance]
-      lat = params[:lat]
-      lng = params[:lng]
+class Api::PlacesController < ApplicationController
+  def suggest
+    # クエリパラメータから値を取得
+    category = params[:category]
+    distance = params[:distance]
+    lat = params[:lat]
+    lng = params[:lng]
 
-      # Todo: 取得APIの記述
+    # Todo: 取得APIの記述
 
-      response = {
-        place_id: "ChIJw76RQYuMGGARHeYoDcNpsZQ",
-        latitude: 35.68765719762984,
-        longitude: 139.7294224868288
-      }
+    response = {
+      place_id: "ChIJw76RQYuMGGARHeYoDcNpsZQ",
+      latitude: 35.68765719762984,
+      longitude: 139.7294224868288
+    }
 
-      render json: response
-    end
+    render json: response
+  end
+  def arrive
+    item = {
+      id: 123456,
+      category: "hair",
+      name: "エメラルドグリーンのツインテール",
+      image_url: "https://yukky-sandbox.dev/_astro/glaceon.DPjrUh7m_Z1iLFcK.webp",
+    }
+    response = {
+      item:,
+      rank: 8,
+      until_next_rank: 180,
+      get_rank_point: 100
+    }
+    render json: response
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     resources :places, only: [] do
       collection do
         get 'suggest'
+        post 'arrive'
       end
-    end 
+    end
   end
+end
