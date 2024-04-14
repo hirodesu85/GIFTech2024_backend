7183321eeabd9d3918d9c60210c24ea774d881ad
いね <133568722+iineineno03k@users.noreply.github.com>
2024-04-14 23:32:04 +0900

    目的地提案APIのControllerを作成 (#6)
    * 目的地提案APIのControllerを作成
    
    * レビュー指摘事項の修正
    
    * 場所関連のAPIを統合
    
    ---------
    
    Co-authored-by: Yukky <67864326+Yoshino-Yukitaro@users.noreply.github.com>

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -1,4 +1,21 @@
 class Api::PlacesController < ApplicationController
+  def suggest
+    # クエリパラメータから値を取得
+    category = params[:category]
+    distance = params[:distance]
+    lat = params[:lat]
+    lng = params[:lng]
+
+    # Todo: 取得APIの記述
+
+    response = {
+      place_id: "ChIJw76RQYuMGGARHeYoDcNpsZQ",
+      latitude: 35.68765719762984,
+      longitude: 139.7294224868288
+    }
+
+    render json: response
+  end
   def arrive
     item = {
       id: 123456,
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :places, only: [] do
       collection do
+        get 'suggest'
         post 'arrive'
       end
     end  
