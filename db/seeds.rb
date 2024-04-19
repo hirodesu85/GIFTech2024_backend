5e627f234d185169185b57731006d5119d3026da
吉野敬太郎 <yoshinoyukitarou@yoshinokeitarounoMacBook-Pro.local>
2024-04-19 20:46:28 +0900

    feat: 美少女のシードデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# 後でfind_or_create_by!に変更する
+
 Bottom.transaction do
   Bottom.delete_all
   Bottom.create!(id: 1, name: "サンプル(後で消す)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/bottoms/toy_omocha_asobu_boy.webp", is_mine: true)
@@ -20,4 +22,9 @@ Top.transaction do
   Top.delete_all
   Top.create!(id: 1, name: "サンプル(後で消す)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/top/toy_omocha_asobu_boy.webp", is_mine: true)
   Top.create!(id: 2, name: "サンプル2(後で消す)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/top/d.webp", is_mine: false)
+end
+
+BeautyGirl.transaction do
+  BeautyGirl.delete_all
+  BeautyGirl.create!(id: 1,rank: 8, until_next_rank: 300, hairs_id: 1, shoes_id: 1, tops_id: 1, bottoms_id: 1)
 end
