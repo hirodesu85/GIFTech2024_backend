b69212d8f8d6f9152f32f1aebdf16162e1224ec2
吉野敬太郎 <yoshinoyukitarou@yoshinokeitarounoMacBook-Pro.local>
2024-04-25 18:52:38 +0900

    feat: とりあえずアイテム名だけ反映した(わかってる範囲で)

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,26 +2,26 @@
 
 Bottom.transaction do
   Bottom.delete_all
-  Bottom.create!(id: 1, name: "サンプル(後で消す)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/bottoms/toy_omocha_asobu_boy.webp", is_mine: true)
+  Bottom.create!(id: 1, name: "キュートなスカート(青)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/bottoms/toy_omocha_asobu_boy.webp", is_mine: true)
   Bottom.create!(id: 2, name: "サンプル2(後で消す)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/bottoms/d.webp", is_mine: false)
 end
 
 Hair.transaction do
   Hair.delete_all
-  Hair.create!(id: 1, name: "サンプル(後で消す)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/hairs/toy_omocha_asobu_boy.webp", is_mine: true)
+  Hair.create!(id: 1, name: "キュートな髪型", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/hairs/toy_omocha_asobu_boy.webp", is_mine: true)
   Hair.create!(id: 2, name: "サンプル2(後で消す)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/hairs/d.webp", is_mine: false)
 end
 
 Shoe.transaction do
   Shoe.delete_all
-  Shoe.create!(id: 1, name: "サンプル(後で消す)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/shoes/toy_omocha_asobu_boy.webp", is_mine: true)
-  Shoe.create!(id: 2, name: "サンプル2(後で消す)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/shoes/d.webp", is_mine: false)
+  Shoe.create!(id: 1, name: "キュートなシューズ(白)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/shoes/toy_omocha_asobu_boy.webp", is_mine: true)
+  Shoe.create!(id: 2, name: "キュートなシューズ(黄)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/shoes/d.webp", is_mine: false)
 end
 
 Top.transaction do
   Top.delete_all
-  Top.create!(id: 1, name: "サンプル(後で消す)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/tops/toy_omocha_asobu_boy.webp", is_mine: true)
-  Top.create!(id: 2, name: "サンプル2(後で消す)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/tops/d.webp", is_mine: false)
+  Top.create!(id: 1, name: "キュートなスカジャン", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/tops/toy_omocha_asobu_boy.webp", is_mine: true)
+  Top.create!(id: 2, name: "キュートなパーカー", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/tops/d.webp", is_mine: false)
 end
 
 BeautyGirl.transaction do
