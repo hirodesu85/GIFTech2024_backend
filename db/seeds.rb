b13921d7b9065b9f8a091951aa0fa3f8b4408506
吉野敬太郎 <yoshinoyukitarou@yoshinokeitarounoMacBook-Pro.local>
2024-04-26 20:01:09 +0900

    feat: seedに作成されたアイテムを反映

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,20 @@ end
 
 Hair.transaction do
   Hair.delete_all
-  Hair.create!(id: 1, name: "キュートな髪型", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/hairs/toy_omocha_asobu_boy.webp", is_mine: true)
-  Hair.create!(id: 2, name: "サンプル2(後で消す)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/hairs/d.webp", is_mine: false)
+  Hair.create!(id: 1, name: "キュートな髪型(ピンク)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/hairs/Icon_HP.webp", is_mine: true)
+  Hair.create!(id: 2, name: "キュートな髪型(青)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/hairs/Icon_HB.webp", is_mine: false)
 end
 
 Shoe.transaction do
   Shoe.delete_all
-  Shoe.create!(id: 1, name: "キュートなシューズ(白)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/shoes/toy_omocha_asobu_boy.webp", is_mine: true)
-  Shoe.create!(id: 2, name: "キュートなシューズ(黄)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/shoes/d.webp", is_mine: false)
+  Shoe.create!(id: 1, name: "キュートなシューズ(黄)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/shoes/Icon_SY.webp", is_mine: true)
+  Shoe.create!(id: 2, name: "キュートなシューズ(白)", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/shoes/Icon_SW.webp", is_mine: false)
 end
 
 Top.transaction do
   Top.delete_all
-  Top.create!(id: 1, name: "キュートなスカジャン", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/tops/toy_omocha_asobu_boy.webp", is_mine: true)
-  Top.create!(id: 2, name: "キュートなパーカー", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/tops/d.webp", is_mine: false)
+  Top.create!(id: 1, name: "キュートなパーカー", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/tops/Item_Pink.webp", is_mine: true)
+  Top.create!(id: 2, name: "キュートなスカジャン", image_url: "https://pub-2da3b55c4b23440c8f76b2b22e4db727.r2.dev/tops/Item_Blue.webp", is_mine: false)
 end
 
 BeautyGirl.transaction do
